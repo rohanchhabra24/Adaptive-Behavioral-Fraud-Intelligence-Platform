55747e51bd037801d7acfadb950eabb1fcdee102
Aditiraj_21BLC1094 <rohanchhabra1906@gmail.com>
2026-06-10 21:58:39 +0530

    Add missing flink-json format dependency to Flink SQL pipeline

--- flink-jobs/src/unified_pipeline.py
+++ flink-jobs/src/unified_pipeline.py
@@ -7,7 +7,7 @@ def main():
     settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
     t_env = StreamTableEnvironment.create(env, environment_settings=settings)
     
-    t_env.get_config().get_configuration().set_string("pipeline.jars", "file:///opt/flink/lib/flink-sql-connector-kafka-1.17.1.jar")
+    t_env.get_config().get_configuration().set_string("pipeline.jars", "file:///opt/flink/lib/flink-sql-connector-kafka-1.17.1.jar;file:///opt/flink/lib/flink-json-1.17.1.jar")
     
     kafka_broker = os.getenv("KAFKA_BROKER", "kafka:29092")
 
