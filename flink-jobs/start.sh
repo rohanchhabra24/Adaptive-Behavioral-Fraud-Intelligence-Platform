693ef634460068e963c3cc33cd9dc4e9737fa6bd
Aditiraj_21BLC1094 <rohanchhabra1906@gmail.com>
2026-06-10 21:46:51 +0530

    Fix Flink JobManager startup crash and healthcheck

--- flink-jobs/start.sh
+++ flink-jobs/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Waiting for Flink JobManager to fully boot..."
-while ! curl -s http://jobmanager:8081/config > /dev/null; do
+while ! wget -qO- http://jobmanager:8081/config > /dev/null; do
   echo "JobManager not ready yet. Retrying in 5 seconds..."
   sleep 5
 done
